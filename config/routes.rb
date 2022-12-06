ed4176dd146f69141236426dd60ec2367d3e6804
qirpi <qirpi@lambd.ax>
2022-12-06 11:28:57 +0100

    make routing better

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root 'webpages#index'
 
   resources :users
-  resources :webpages
-
+  resources :webpages do
+    get 'toggle_read_status', on: :member
+    get 'show_read', on: :collection
+  end
+  
   get '/signup', to: 'users#new'
+  
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
-  root 'webpages#index'
-
-  get '/webpages/:id/toggle_read_status', to: 'webpages#toggle_read_status', as: 'toggle_read_status'
-  get '/show_read', to: 'webpages#show_read', as: 'show_read'
 
   # API
   namespace :api do
