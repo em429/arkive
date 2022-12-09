115fc1697412a693e33de0b516e02eb9defc83db
qirpi <qirpi@lambd.ax>
2022-12-09 01:04:37 +0100

    refresh fly deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.active_record.sqlite3_production_warning=false
+  
 end
