50e43f2eea89a38979c52fae751465e87acbec2d
qirpi <qirpi@lambd.ax>
2022-12-15 04:51:15 +0100

    revert

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {
@@ -27,5 +28,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require('flowbite/plugin'),
   ]
 }
