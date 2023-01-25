11401266822fbf67a92b03168aa97fcb4de9721b
qirpi <qirpi@lambd.ax>
2023-01-25 23:28:41 +0100

    remove api temporarily

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
-  # API
-  namespace :api do
-    namespace :v1 do
-      resources :webpages, only: :create
-    end
-  end
+  
 end
