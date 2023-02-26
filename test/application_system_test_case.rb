7ad3897ef47eb4c260444af9ad7dd5e5638614b9
qirpi <qirpi@lambd.ax>
2023-02-26 13:02:14 +0100

    add, not working?

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,15 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  def capybara_log_in_as(user, password: 'asdf1234')
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: password
+    click_button "Log in"
+    # magic_test
+    assert page.has_content? 'Private Web Archive'
+  end
+  
 end
+
+
