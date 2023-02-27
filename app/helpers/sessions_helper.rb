b9317211935e123d8150875160fc19f7129a8bd1
qirpi <qirpi@lambd.ax>
2023-02-27 21:42:32 +0100

    correct

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
   def render_account_dropdown
     if current_user
       render 'users/account_dropdown'
-    elsif current_page?('/login')
+    elsif current_page?(login_path)
       ""
     else
       link_to "Log in", login_path, class:"text-slate-800 hover:text-slate-500"
